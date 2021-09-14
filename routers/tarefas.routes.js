f9e27c8df905658e970958dc6eed3ec4f94d0a90
Flavio Wu <flaviowu@gmail.com>
2021-09-14 03:05:30 -0300

    add mod sort

--- routers/tarefas.routes.js
+++ routers/tarefas.routes.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const router = express.Router();
 const Tarefa = require("../models/tarefa");
+const ordenar = require("../modules/sort");
 
 router.get("/", async (req, res) => {
   await Tarefa.find({})
     .then((tarefa) => {
+      res.status(200).send(tarefa);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send("Não foi possível fazer sua requisição. Tente novamente.");
+      console.error(err);
+    });
+});
+
+router.get("/sortByPrioridade", async (req, res) => {
+  await Tarefa.find({})
+    .then((tarefa) => {
+      tarefa = ordenar.porPrioridade(tarefa);
+      res.status(200).send(tarefa);
+    })
+    .catch((err) => {
+      res
+        .status(400)
+        .send("Não foi possível fazer sua requisição. Tente novamente.");
+      console.error(err);
+    });
+});
+
+router.get("/sortByPrazo", async (req, res) => {
+  await Tarefa.find({})
+    .then((tarefa) => {
+      tarefa = ordenar.porPrazo(tarefa);
       res.status(200).send(tarefa);
     })
     .catch((err) => {
@@ -42,25 +71,25 @@ router.post("/add", async (req, res) => {
 });
 
 router.put("/update/:id", async (req, res) => {
-    await Tarefa.updateOne( { _id: req.params.id } , req.body ) 
-      .then(() => {
-        res.status(200).send("Tarefa atualizada com sucesso");
-      })
-      .catch((err) => {
-        res.status(400).send("Algo deu errado na requisição, tente novamente");
-        console.error(err);
-      });
-  });
+  await Tarefa.updateOne({ _id: req.params.id }, req.body)
+    .then(() => {
+      res.status(200).send("Tarefa atualizada com sucesso");
+    })
+    .catch((err) => {
+      res.status(400).send("Algo deu errado na requisição, tente novamente");
+      console.error(err);
+    });
+});
 
-  router.delete("/delete/:id", async (req, res) => {
-    await Tarefa.deleteOne({ _id: req.params.id })
-      .then(() => {
-        res.status(200).send(`Tarefa excluída com sucesso`);
-      })
-      .catch((err) => {
-        res.status(400).send("Algo deu errado na requisição, tente novamente");
-        console.error(err);
-      });
-  });
+router.delete("/delete/:id", async (req, res) => {
+  await Tarefa.deleteOne({ _id: req.params.id })
+    .then(() => {
+      res.status(200).send(`Tarefa excluída com sucesso`);
+    })
+    .catch((err) => {
+      res.status(400).send("Algo deu errado na requisição, tente novamente");
+      console.error(err);
+    });
+});
 
 module.exports = router;
